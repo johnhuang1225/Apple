f0d87dff3bede1c8fd93acfdcaf15ec8a9654dde
John Huang <johnhuang1225@gmail.com>
2015-11-28 23:57:10 +0800

    add HackWithSwift Project 12

---
+++ iOS/Practice Project/23 HackingWithSwfit/Project 12 - NSUserDefaults/Project 10 - Names to Faces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Project 10 - Names to Faces
+//
+//  Created by John Huang on 2015/11/28.
+//  Copyright © 2015年 JohnHuang. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
